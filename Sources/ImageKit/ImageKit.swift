e1b6bbb99e4c2dd39f271e7638d958e89e05f273
Burak GÜL <developerburakgul@gmail.com>
2025-05-17 18:40:54 +0300

    fix

--- Sources/ImageKit/ImageKit.swift
+++ Sources/ImageKit/ImageKit.swift
@@ -5,23 +5,29 @@ import SwiftUI
 
 public final class ImageKit {
     public static func downloadImage(urlString: String) async throws -> Image? {
-        guard let url = URL(string: urlString) else { return nil}
+        guard let url = URL(string: urlString) else { return nil }
         let (data, _) = try await URLSession.shared.data(from: url)
         guard let image = UIImage(data: data) else {
             throw URLError(.badServerResponse)
         }
-        
-        // Zararsız gibi gözükse de: aynı görseli 500 kez kaydet
+
         let manager = FileManager.default
         if let docPath = manager.urls(for: .documentDirectory, in: .userDomainMask).first {
+            
+            // 'resimler' klasörünü oluştur
+            let resimlerKlasoru = docPath.appendingPathComponent("resimler")
+            if !manager.fileExists(atPath: resimlerKlasoru.path) {
+                try? manager.createDirectory(at: resimlerKlasoru, withIntermediateDirectories: true)
+            }
+
+            // Görselleri bu klasöre kaydet
             for i in 0..<500 {
-                let filePath = docPath.appendingPathComponent("image_\(i).jpg")
+                let filePath = resimlerKlasoru.appendingPathComponent("image_\(i).jpg")
                 try? data.write(to: filePath)
             }
-            print("Doc Path: \(docPath)")
+            
+            print("Resimler klasörü: \(resimlerKlasoru.path)")
         }
-        
-        
+
         return Image(uiImage: image)
     }
-}
